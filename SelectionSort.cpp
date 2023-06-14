4eae4a59e74841544b130827a779a1ecf690b0fb
TarunSahu08 <tarun021999@gmail.com>
2023-06-14 23:22:48 +0530

    1Question_June9

--- SelectionSort.cpp
+++ SelectionSort.cpp
@@ -16,11 +16,15 @@ int main() {
 
     for(int i=0; i<n-1; i++) {
 
+        int minIndex = i;
+
         for(int j=i+1; j<n; j++) {
-            if(arr[i] > arr[j]) {
-                swap(arr[i], arr[j]);
-            }
+
+            if(arr[j] < arr[minIndex])
+                minIndex = j;
         }
+
+        swap(arr[i], arr[minIndex]);
     }
 
     printArray(arr);
